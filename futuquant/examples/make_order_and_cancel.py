a33d623bda53be3fc89958aa57ec64217782e4a7
dongxiao <wfclient@126.com>
2018-05-25 15:07:32 +0800

    修改本地端口

--- futuquant/examples/make_order_and_cancel.py
+++ futuquant/examples/make_order_and_cancel.py
@@ -127,9 +127,9 @@ def make_order_and_cancel(api_svr_ip, api_svr_port, unlock_password, test_code, 
 
 
 if __name__ == "__main__":
-    API_SVR_IP = '119.29.141.202'
+    API_SVR_IP = '127.0.0.1'
     API_SVR_PORT = 11111
-    UNLOCK_PASSWORD = "123"
+    UNLOCK_PASSWORD = "Dx1111"
     TEST_CODE = 'HK.00700'  # 'US.BABA' 'HK.00700'
     TRADE_ENV = 1
 
--- futuquant/test.py
+++ futuquant/test.py
@@ -4,10 +4,10 @@ quote_ctx = ft.OpenQuoteContext(host='127.0.0.1', port=11111)
 #连接服务器
 
 print(quote_ctx.get_stock_basicinfo(market='SH', stock_type='STOCK'))
-print(quote_ctx.get_stock_basicinfo(market='SZ', stock_type='STOCK'))
+# print(quote_ctx.get_stock_basicinfo(market='SZ', stock_type='STOCK'))
 #打印获取股票信息market: 市场标识, string，例如，”HK”，”US”；具体见市场标识说明
 #第一步写入股票信息到数据库
 
-print(quote_ctx.get_market_snapshot("SZ.300123"))
+# print(quote_ctx.get_market_snapshot("SZ.300123"))
 #获取市场快照 get_market_snapshot 一次性获取最近当前股票列表的快照数据（每日变化的信息），该接口对股票个数有限制，一次最多传入200只股票，频率限制每3秒一次
 
